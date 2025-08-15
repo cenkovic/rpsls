4e17694a2cde4753a61d6bd84fbc8095b42e730a
Nemanja Cenkovic <cenkovic.nemanja@gmail.com>
2025-08-15 13:58:03 +0200

    Memo choices promise

--- apps/rpsls/app/routes/game.tsx
+++ apps/rpsls/app/routes/game.tsx
@@ -1,9 +1,10 @@
 import './game.css';
-import { cache, Suspense } from 'react';
+import { cache, Suspense, useMemo } from 'react';
 import { gameService } from '../services/game-service';
 import { GameChoices } from '../components/GameChoices/GameChoices';
 
 export default function Game() {
+  const choicesPromise = useMemo(() => cache(gameService.getChoices), []);
   return (
     <div id={'game-container'}>
       <h1 id={'game-title'}>RPSLS Game</h1>
@@ -12,7 +13,7 @@ export default function Game() {
       </p>
 
       <Suspense fallback={'Loading choices...'}>
-        <GameChoices choices={cache(gameService.getChoices)()} />
+        <GameChoices choices={choicesPromise()} />
       </Suspense>
     </div>
   );
